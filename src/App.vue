7cfe167de5b747106278847eb1e59a76d27a07fa
Christoffer Seljehaug <cs_seljehaug@hotmail.com>
2018-11-17 18:41:36 +0100

    setup and API configuration

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+   <div id="app">
+      {{ API_KEY }}
+      {{ API_SECRET }}
+   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+   import API_CONFIG from '../API_CONFIG.js';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+   export default {
+      name: 'app',
+      components: {
+
+      },
+      data: function() {
+         return {
+            API_KEY: null,
+            API_SECRET: null
+         };
+      },
+      created() {
+         this.API_KEY = API_CONFIG.API_KEY;
+         this.API_SECRET = API_CONFIG.API_SECRET;
+      }
+   };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+   #app {
+      font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+      margin-top: 60px;
+   }
 </style>
